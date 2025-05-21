a4e2cc2f02a21dff0b3bc3323f4c4c573f35705e
Nicolò Pincelli <pincelli.n@gmail.com>
2025-05-21 12:18:19 +0200

    feat:  add  ListNode and LinkedList class

---
+++ linked-list.ts
@@ -0,0 +1,23 @@
+class ListNode<T> {
+  next?: ListNode<T>;
+  constructor(value: T) {}
+}
+
+class LinkedList<T> {
+  private root?: ListNode;
+  private length = 0;
+
+  add(value: T) {
+    const node = new ListNode(value);
+    if (!this.root) {
+      this.root = node;
+    } else {
+      let current = this.root;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    this.length++;
+  }
+}
